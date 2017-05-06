f85991c94c780e550a8a4e499b2b6dd8f78af1aa
Buddy Arifin <buddy.arifin@olx.co.id>
2017-05-06 07:27:27 +0700

    add more test

--- MyLifeUITests/MyLifeUITests.swift
+++ MyLifeUITests/MyLifeUITests.swift
@@ -30,6 +30,8 @@ class MyLifeUITests: XCTestCase {
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        XCUIApplication().terminate()
         super.tearDown()
     }
     
